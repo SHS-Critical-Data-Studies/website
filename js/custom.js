40375dfa823f3dc78619b3227b69928d7c150d2a
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-05-16 22:07:54 +0200

    Beginning of analysis

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+function changeWordcloud(){
+	var list = ['UK', 'Morning Show', 'Sciences', 'USA', 'Ukraine-Russia News', 'International Affairs', 'Ukraine-Russia war', 'Music & New York City', 'Trevor Noah Show', 'Business News', 'Education', 'Ted Talks', 'World News', 'Education', 'Biathlon']
+	var val = $("#wordcloud").val()-1
+	$("#wordcloud_title").text("Topic  "+ (val+1)+ " : "+list[val])
+	$("#wordcloud_img").attr("src", "images/word_cloud_topic_"+val+".png") 
+
+}
+
+function changeDisplayRun(){
+	var val = $("#displayrun").val()
+	$("#wordcloud_img").attr("src", "images/wc"+val+".png") 
+
+}
