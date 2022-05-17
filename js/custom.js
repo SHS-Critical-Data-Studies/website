c0fb435a2b8682cd7ac750be0209e7e91c38c15c
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2022-05-17 16:17:07 +0200

    updated viz

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 function changeWordcloud(){
-	var list = ['UK', 'Morning Show', 'Sciences', 'USA', 'Ukraine-Russia News', 'International Affairs', 'Ukraine-Russia war', 'Music & New York City', 'Trevor Noah Show', 'Business News', 'Education', 'Ted Talks', 'World News', 'Education', 'Biathlon']
+	var list = ['International Affairs on Nuclear', 'World News', 'Documentaries on World Crisis', 'News', 'Ukraine-Russia News', 'Ted Talks', 'Johnny Depp-Amber Heard', 'Health', 'White House', 'Music', 'Education', 'Politics', 'Noah Trevor', 'Morning News', 'Rathee Dhruv']
 	var val = $("#wordcloud").val()-1
 	$("#wordcloud_title").text("Topic  "+ (val+1)+ " : "+list[val])
 	$("#wordcloud_img").attr("src", "images/word_cloud_topic_"+val+".png") 
